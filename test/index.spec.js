3aac2922a899870bc8e893ac320d7c317e86810e
janarf <janarf@hotmail.com>
2019-04-26 15:39:01 -0300

    Tests completed: empty string test, cpf with letter.

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,7 +8,13 @@ describe('cpfValidator()', () => {
   it('Return false for equal digits CPF', () => {
     expect(tools.cpfValidator('11111111111')).to.be.false;
   });
-  it('Return false for unvalid CPF', () => {
+  it('Return false for invalid CPF', () => {
     expect(tools.cpfValidator('06944530677')).to.be.false;
+  });
+  it('Return false for string with letters', () => {
+    expect(tools.cpfValidator('069445aa677')).to.be.false;
+  });
+  it('Return false for empty string', () => {
+    expect(tools.cpfValidator('')).to.be.false;
   });
 });
